108306b87c840681a8a5e1fd3f262531852a4ef3
chrisuzuki62 <chrisuzuki62@gmail.com>
2022-01-13 10:12:59 -0800

    output error fix

--- Lab0/docs/search/files_0.js
+++ Lab0/docs/search/files_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['test_2epy_0',['test.py',['../test_8py.html',1,'']]]
+  ['main_2epy_0',['main.py',['../main_8py.html',1,'']]]
 ];
--- Lab0/docs/search/functions_0.js
+++ Lab0/docs/search/functions_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['the_5fanswer_0',['the_answer',['../test_8py.html#a4a1ccb012dccffc254e8dc72bdff662d',1,'test']]]
+  ['led_5fbrightness_0',['led_brightness',['../namespacemain.html#a2d7970668187bfad392eb768713be06a',1,'main']]],
+  ['led_5fsetup_1',['led_setup',['../namespacemain.html#a6ef5ac3570ce2c3589413b978e830e1d',1,'main']]]
 ];
