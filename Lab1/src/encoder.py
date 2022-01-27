16be07a4a97b5077c55e8e4e18e504772dc6d90d
damondli <dli48@calpoly.edu>
2022-01-26 22:54:55 -0800

    Initial commit for submission

--- Lab1/src/encoder.py
+++ Lab1/src/encoder.py
@@ -1,4 +1,5 @@
-'''!@file encoder.py
+'''!
+    @file encoder.py
     @brief A driver for reading from Quadrature Encoders
     @details This file creates an encoder calss that contains 3 methods that can
              be used in other files to get the change of the position of an
@@ -15,7 +16,8 @@ import pyb
 import time
 
 class Encoder:
-    ''' @brief Interface with quadrature encoders
+    '''!
+        @brief Interface with quadrature encoders
         @details Creates a class that can be called into other python files that
                  is used to interface and read out the position of an encoder.
                  The class contains 3 methods. One to construct an encoder object,
@@ -25,12 +27,15 @@ class Encoder:
 
     def __init__(self, enc_num):
         
-        ''' @brief Constructs an encoder object
+        '''!
+            @brief Constructs an encoder object
             @details Instantiates an encoder object that contains 2 different
                      methods that can be used in other python files. This also
                      generally sets up the use of any encoder so multiple can 
                      be called in any file.
-            @param enc_num
+            @param enc_num Value used to establish each instance of the encoder.
+                           Currently, this driver only supports up to 2 encoders.
+                           Therefore this argument is either 1 or 2.
         '''
         ## Sets the maximum amount of ticks that the encoder can record. This
         ## is exactly the same as the maximum amount of bytes that the encoder
@@ -39,9 +44,13 @@ class Encoder:
         
         ## Defines the two encoders that will be used and seperates usage
         if enc_num == 1:
+            
+            ## Sets the appropriate timer for encoder 1
             self.timer = pyb.Timer(4, prescaler = 0, period = self.period)
-        
+            
+            ## Sets the appropriate pin 1 for encoder 1
             self.Pin1 = pyb.Pin(pyb.Pin.cpu.B6)
+            ## Sets the appropriate pin 2 for encoder 1
             self.Pin2 = pyb.Pin(pyb.Pin.cpu.B7)
         
             ## Sets one channel that the encoder will use to track time
@@ -51,9 +60,13 @@ class Encoder:
             self.ch2 = self.timer.channel(2,pyb.Timer.ENC_AB, pin=self.Pin2)
 
         elif enc_num == 2:
+            
+            ## Sets the appropriate timer for encoder 2
             self.timer = pyb.Timer(8, prescaler = 0, period = self.period)
             
+            ## Sets the appropriate pin 1 for encoder 2
             self.Pin1 = pyb.Pin(pyb.Pin.cpu.C6)
+            ## Sets the appropriate pin 2 for encoder 2
             self.Pin2 = pyb.Pin(pyb.Pin.cpu.C7)
             
             ## Sets one channel that the encoder will use to track time
@@ -62,18 +75,21 @@ class Encoder:
             ## Sets one channel that the encoder will use to track time
             self.ch2 = self.timer.channel(2,pyb.Timer.ENC_AB, pin=self.Pin2)
             
-        ## variables that record the ticks of encoder
+        ## Variable to represent the previous tick counter
         self.pos_1 = self.timer.counter()
         
+        ## Variable to represent the current tick counter
         self.pos_2 = self.timer.counter()
         
+        ## Variable to represent the current position
         self.current_pos = 0
 
     def read(self):
-        ''' @brief Updates encoder position 
+        '''!
+            @brief Updates encoder position 
             @details Creates an update method that when ran will update the position
                      of the specfied encoder
-            @return returns the position of the encoder
+            @return Returns the position of the encoder
         '''
         ## Defines the position of the encoder as the the timer linked to the encoder
         self.pos_1 = self.pos_2
@@ -94,7 +110,8 @@ class Encoder:
         return self.current_pos
     
     def zero(self):
-        ''' @brief Zeros encoder position
+        '''!
+            @brief Zeros encoder position
             @details Creates a method that when called sets the position of the
                      encoder to zero.
         '''
